ca0f6e6e69de78d352106eefa7501c600ef01c30
onifade-joshua <onifadejoshua15@gmail.com>
2023-09-13 16:26:15 +0100

    Add project files.

---
+++ deepDiveWithHeapAndStack/HeapAndStack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deepDiveWithHeapAndStack
+{
+    public class HeapAndStack
+    {
+        public int JustGiven(int size)
+        {
+            int outcomeResult;
+            outcomeResult = size + 10;
+            return outcomeResult;
+            
+        }
+    }
+}
---
+++ deepDiveWithHeapAndStack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deepDiveWithHeapAndStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int actualValue = 5;
+            string stringValue = "Hey you!";
+            bool boolValue = false;
+            byte byteValue = 0;
+
+            HeapAndStack HeapStack = new HeapAndStack();
+            HeapStack.JustGiven(2);
+        }
+    }
+}
